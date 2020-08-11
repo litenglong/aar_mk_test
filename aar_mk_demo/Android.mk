41308a95d370138d4b3598347fc34b224abd2083
litenglong <572309967@qq.com>
2020-08-11 17:20:48 +0800

    modify

--- aar_mk_demo/Android.mk
+++ aar_mk_demo/Android.mk
@@ -1,13 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := upplayer0.0.6:$(LOCAL_PATH)/libs/upplayer0.0.6.aar \
-										appcompat-v7:$(LOCAL_PATH)/libs/appcompat-v7-26.1.0.aar \
-										support-fragment-26.1.0-sources:$(LOCAL_PATH)/libs/support-fragment-26.1.0-sources.jar \
-										constraint-layout:$(LOCAL_PATH)/libs/constraint-layout.jar \
-										ijkplayer-exo-0.8.8:$(LOCAL_PATH)/libs/ijkplayer-exo-0.8.8.jar \
-										ijkplayer-java-0.8.8:$(LOCAL_PATH)/libs/ijkplayer-java-0.8.8.jar \
-										eventbus-3.0.0-sources:$(LOCAL_PATH)/libs/eventbus-3.0.0-sources.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := upplayer0.0.6:libs/upplayer0.0.6.aar \
+										appcompat-v7:libs/android-support-v7-appcompat.jar \
+										support-fragment-26.1.0-sources:libs/support-fragment-26.1.0-sources.jar \
+										constraint-layout:libs/constraint-layout.jar \
+										ijkplayer-exo-0.8.8:libs/ijkplayer-exo-0.8.8.jar \
+										ijkplayer-java-0.8.8:libs/ijkplayer-java-0.8.8.jar \
+										eventbus-3.0.0-sources:libs/eventbus-3.0.0-sources.jar \
+										android:libs/android.jar \
+										android-support-v4:libs/android-support-v4.jar
 include $(BUILD_MULTI_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -18,19 +20,20 @@ LOCAL_STATIC_JAVA_LIBRARIES := support-fragment-26.1.0-sources \
 								constraint-layout \
 								ijkplayer-exo-0.8.8 \
 								ijkplayer-java-0.8.8 \
-								eventbus-3.0.0-sources
+								eventbus-3.0.0-sources \
+								android \
+								android-support-v4 \
+								appcompat-v7
 
 
-LOCAL_STATIC_JAVA_AAR_LIBRARIES := upplayer0.0.6 \
-									appcompat-v7
+LOCAL_STATIC_JAVA_AAR_LIBRARIES := upplayer0.0.6
 
 
 
 										
 LOCAL_AAPT_FLAGS := --auto-add-overlay \
                    --extra-packages com.ny.ijk.upplayer.media.PlayerManager \
-					--extra-packages com.ny.ijk.upplayer.ui.PlayActivity \
-					--extra-packages android.support.v7.appcompat 
+					--extra-packages com.ny.ijk.upplayer.ui.PlayActivity 
 									
 LOCAL_PACKAGE_NAME := AiTV_demo
 									
--- aar_mk_demo/java/com/freevisiontech/iqy/aitv/MainActivity.java
+++ aar_mk_demo/java/com/freevisiontech/iqy/aitv/MainActivity.java
@@ -25,21 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private String url_1 = "http://hd.yinyuetai.com/uploads/videos/common/410E01536EE4913B861AAAE14384E5E5.flv?sc=c7f63ff8b1851298&br=1101&vid=2113588&aid=273&area=HT&vst=0";
     private String url_2 = "http://he.yinyuetai.com/uploads/videos/common/80910161684BA0A34DE07502D4029DB8.mp4?sc=b5138673890caad4&br=2686&vid=3151952&aid=22&area=HT&vst=0";
     private String url_3 = "http://he.yinyuetai.com/uploads/videos/common/4DD10169F6DF359A0590C808DA7AAC62.mp4?sc=b1a1bd1dcd93d15e&br=3150&vid=3372051&aid=22&area=HT&vst=3";
-//    private String url_1 = "/storage/emulated/0/DCIM/fvmobile/VID20190301165551460TimeLapse.mp4";
-//    private String url_2 = "/storage/emulated/0/DCIM/fvmobile/VID20190301152631562TimeLapse.mp4";
-//    private String url_3 = "/storage/emulated/0/DCIM/fvmobile/VID20190301143809284TimeLapse.mp4";
-//    private String url_1 = "http://ivi.bupt.edu.cn/hls/cctv1hd.m3u8";
-//    private String url_2 = "http://ivi.bupt.edu.cn/hls/cctv5phd.m3u8";
-//    private String url_3 = "http://ivi.bupt.edu.cn/hls/cctv6hd.m3u8";
-//    private String url_4 = "/storage/emulated/0/DCIM/fvmobile/VID20181213141537294TimeLapse.mp4";
-//    private String url_5 = "/storage/emulated/0/DCIM/fvmobile/VID20181213141828022.mp4";
-//    private String url_6 = "/storage/emulated/0/DCIM/fvmobile/VID20181213141903259TimeLapse.mp4";
-//    private String url_7 = "/storage/emulated/0/DCIM/fvmobile/VID20181213142031044.mp4";
-//    private String url_8 = "/storage/emulated/0/DCIM/fvmobile/VID20181213142057210TimeLapse.mp4";
-//    private String url_9 = "/storage/emulated/0/DCIM/fvmobile/test_video1.mp4";
-//    private String url_10 = "/storage/emulated/0/DCIM/fvmobile/test_video2.mp4";
-//    private String url_11 = "/storage/emulated/0/DCIM/fvmobile/VID20181215163356526.mp4";
-    ArrayList<String> urls = new ArrayList<>();
+    ArrayList<String> urls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
